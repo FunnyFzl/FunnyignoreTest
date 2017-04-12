986a44744edb375f3b898ab40f3a88994f4568c4
fengzl <fengzl@youyoubaoxian.com>
2017-04-12 11:46:08 +0800

    Initial Commit

---
+++ ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by Funny on 2017/4/12.
+//  Copyright © 2017年 Funny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
